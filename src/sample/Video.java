fa16388010765c9aaabf86b7c81cf7978ee3886d
Angelo Heringa <48793053+AngeloHeringa@users.noreply.github.com>
2021-05-26 13:40:08 +0200

    helemaal klaar hiermee

--- src/sample/Video.java
+++ src/sample/Video.java
@@ -4,9 +4,33 @@ import java.time.LocalDateTime;
 
 public class Video {
     private final String title;
+    private int ads;
+    private int views;
+
+    public int getAds() {
+        return ads;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
 
     public Video(String title) {
         this.title = title;
+        ads=0;
+    }
+
+    public void setAds(int ads) {
+        this.ads = ads;
+    }
+
+    public Double getAdRevenue(){
+        if (views>=1000&&views<100000){
+            return (ads * views)*0.05;
+        }else if (views >= 100000){
+            return (ads * 100000+((views-100000)*0.5))*0.05;
+        }
+        return 0D;
     }
 
     public Integer maxAds(int minutes, boolean isVerified, boolean featured, boolean explicitContent){
--- src/test/VideoAds.java
+++ src/test/VideoAds.java
@@ -21,4 +21,29 @@ public class VideoAds {
         assertEquals(0, (int) video.maxAds(30,false,true,false));
 
     }
+
+    @Test
+    public void adRevenueTest(){
+        Video video =  new Video("title");
+
+        assertEquals(1,(int)video.maxAds(10, true, false, false));
+        video.setAds(video.maxAds(10, true, false, false)); //ads=1
+
+        video.setViews(0);
+        assertEquals((double) 0, video.getAdRevenue(), 0.0001);
+        video.setViews(1);
+        assertEquals((double) 0, video.getAdRevenue(), 0.0001);
+        video.setViews(999);
+        assertEquals((double) 0, video.getAdRevenue(), 0.0001);
+        video.setViews(1000);
+        assertEquals(50d, video.getAdRevenue(), 0.0001);
+        video.setViews(1001);
+        assertEquals(50.05, video.getAdRevenue(), 0.0001);
+        video.setViews(99999);
+        assertEquals(4999.95, video.getAdRevenue(), 0.0001);
+        video.setViews(100000);
+        assertEquals(5000d, video.getAdRevenue(), 0.0001);
+        video.setViews(100001);
+        assertEquals(5000.025, video.getAdRevenue(), 0.0001);
+    }
 }
